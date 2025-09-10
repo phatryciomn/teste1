0ed7aec7cf63e75e784ab4e24a6cb871aa5f3b40
Phatrycio Miranda <phatrycio137@gmail.com>
2025-09-10 19:15:46 -0300

    adicionando funcao para mudar o background

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ const buttons = document.querySelectorAll("button");
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", createParagraph);
 }
+
+function aplicarCorAleatoria () {
+  const c = '#' + Math.floor(Math.random()*0xFFFFFF).toString(16).padStart(6, '0')
+  document.body.style.background = c
+}
+
+const botao = document.getElementById('btn')
+
+botao.addEventListener('click', aplicarCorAleatoria)
